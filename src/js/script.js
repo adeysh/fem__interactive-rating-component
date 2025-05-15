483efc722dae6a889d7e6962bccc4a15fc12d702
Adesh Katiya <adeshkatiya27@gmail.com>
2025-05-15 16:58:43 +0530

    feat: Improve accessibility in html and styles
    add aria attributes,  add js for toggling the state with keyboard accessibility

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+const ratingForm = document.getElementById("rating-form");
+const ratingCard = document.querySelector(".rating-card--rating-state");
+const thankyouRatingCard = document.querySelector(".rating-card--thankyou-state");
+const selectedRating = document.getElementById("selected-rating");
+const ratingLabels = document.querySelectorAll(".rating-card__rating-label");
+const ratingRadioInputs = document.querySelectorAll(".rating-card__rating-input");
+const errorEl = document.getElementById("error");
+
+function showThankyouState(rating) {
+    ratingCard.classList.toggle("active");
+    thankyouRatingCard.classList.toggle("active");
+    selectedRating.textContent = rating;
+}
+
+function showError() {
+    errorEl.classList.toggle("hidden", false);
+}
+
+ratingForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+
+    if (Object.keys(data).length !== 0) {
+        errorEl.classList.toggle("hidden");
+        showThankyouState(data.rating);
+    } else {
+        showError();
+    }
+});
+
+for (const radio of ratingRadioInputs) {
+    radio.addEventListener("change", () => {
+        errorEl.classList.toggle("hidden", true);
+    });
+}
+
+
+// keyboard accessibility
+ratingForm.addEventListener("keydown", (e) => {
+    if (e.key == " " || e.key == "Enter") {
+        e.target.click();
+    }
+});
